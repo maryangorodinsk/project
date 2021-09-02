1b2ade66d4d2ea4bace7e8b7aef13e7500ddde3a
Maryan <79370035+maryangorodinsk@users.noreply.github.com>
2021-09-02 20:24:04 +0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 def on_forever():
-    basic.show_string("Hello!")
+    basic.show_string("Hello World!")
+    basic.show_leds("""
+        . # # # .
+                # # # # #
+                # # # # #
+                # # # # #
+                . # # # .
+    """)
+    basic.show_icon(IconNames.ANGRY)
+    music.play_melody("- C5 B A E B F - ", 115)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+music.playMelody("E G C5 C5 B E B G ", 120)
 basic.forever(function () {
+    music.playMelody("- C5 B A E B F - ", 115)
     basic.showString("Hello World!")
+    basic.showNumber(1000)
+    basic.showIcon(IconNames.Angry)
     basic.showLeds(`
         . # # # .
         # # # # #
@@ -7,6 +11,4 @@ basic.forever(function () {
         # # # # #
         . # # # .
         `)
-    basic.showIcon(IconNames.Angry)
-    music.playMelody("- C5 B A E B F - ", 115)
 })
