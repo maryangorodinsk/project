4e876f997047b44db0b37877b55b94e8d6700d09
Maryan <79370035+maryangorodinsk@users.noreply.github.com>
2021-09-02 20:36:45 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
 music.playMelody("E G C5 C5 B E B G ", 120)
+basic.showNumber(56312)
+basic.showArrow(ArrowNames.SouthWest)
+music.setBuiltInSpeakerEnabled(true)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Yes)
+}
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    # . # . #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(5000)
 basic.forever(function () {
     music.playMelody("- C5 B A E B F - ", 115)
     basic.showString("Hello World!")
